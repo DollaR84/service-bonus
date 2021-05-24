c2acffea06b47ec37119f3909efa1ed4a94d76a3
DollaR <cooler84@ukr.net>
2021-05-24 23:46:52 +0300

    create bonus serializer

--- bonus/api/serializers.py
+++ bonus/api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from .models import Bonus
+
+
+class BonusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bonus
+        fields = ['balance']
+
 
 class UserSerializer(serializers.ModelSerializer):
+    bonus = BonusSerializer(many=False)
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'bonus']
