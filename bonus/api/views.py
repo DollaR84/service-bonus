a9977cf014cf474a1ea2c9de4391063c3a71afb1
DollaR <cooler84@ukr.net>
2021-05-25 13:58:09 +0300

    create operations views

--- bonus/api/views.py
+++ bonus/api/views.py
@@ -1,29 +1,42 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 
 from . import serializers
+
 from .models import Operation
+
+from .permissions import IsUser, IsAdmin
+
+from . import services
 
 # Create your views here.
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsAdmin]
 
 
 class OperationList(generics.ListCreateAPIView):
-    queryset = Operation.objects.all()
     serializer_class = serializers.OperationSerializer
+    permission_classes = [permissions.IsAuthenticated, IsUser]
+
+    def get_queryset(self):
+        return Operation.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+        method = getattr(services, self.request.data['method'])
+        method(self.request.user, int(self.request.data['count']))
 
 
 class OperationDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Operation.objects.all()
     serializer_class = serializers.OperationSerializer
+    permission_classes = [permissions.IsAuthenticated, IsUser]
