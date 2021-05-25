c5689231373469cdfb2fe51a6c139c015323b4b3
DollaR <cooler84@ukr.net>
2021-05-25 13:57:47 +0300

    add operations urls

--- bonus/api/urls.py
+++ bonus/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('users/<int:pk>/', views.UserDetail.as_view()),
     path('operations/', views.OperationList.as_view()),
     path('operations/<int:pk>/', views.OperationDetail.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
