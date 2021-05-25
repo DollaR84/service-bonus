48f972973863e93f70e5426d4d7015fbbb6e4e99
DollaR <cooler84@ukr.net>
2021-05-25 16:30:28 +0300

    create view balance

--- bonus/api/views.py
+++ bonus/api/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 from . import serializers
 
-from .models import Operation
+from .models import Bonus, Operation
 
-from .permissions import IsUser, IsAdmin
+from .permissions import IsUser
 
 from . import services
 
@@ -14,13 +14,21 @@ from . import services
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
-    permission_classes = [permissions.IsAuthenticated, IsAdmin]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+
+class BonusBalance(generics.ListAPIView):
+    serializer_class = serializers.BonusSerializer
+    permission_classes = [permissions.IsAuthenticated, IsUser]
+
+    def get_queryset(self):
+        return Bonus.objects.filter(user=self.request.user)
 
 
 class OperationList(generics.ListCreateAPIView):
