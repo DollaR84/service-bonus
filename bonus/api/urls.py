880dc3854835d36199c9f0032f76bf4e0ba4b6a5
DollaR <cooler84@ukr.net>
2021-05-25 16:29:50 +0300

    add route to balance method

--- bonus/api/urls.py
+++ bonus/api/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('balance/', views.BonusBalance.as_view()),
     path('operations/', views.OperationList.as_view()),
     path('operations/<int:pk>/', views.OperationDetail.as_view()),
     path('api-auth/', include('rest_framework.urls')),
