bd6994bc38169f0b3e511e6c7088c593f16d9b25
DollaR <cooler84@ukr.net>
2021-05-25 10:19:31 +0300

    create urls for operations

--- bonus/api/urls.py
+++ bonus/api/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('operations/', views.OperationList.as_view()),
+    path('operations/<int:pk>/', views.OperationDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
