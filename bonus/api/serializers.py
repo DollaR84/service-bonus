7c0fe11f8ab86f66c89e15a3903d577a7eb86af0
DollaR <cooler84@ukr.net>
2021-05-25 10:18:32 +0300

    create serializers for operations

--- bonus/api/serializers.py
+++ bonus/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 from .models import Bonus
+from .models import Operation
 
 
 class BonusSerializer(serializers.ModelSerializer):
@@ -12,6 +13,16 @@ class BonusSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     bonus = BonusSerializer(many=False)
+    operations = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'bonus']
+        fields = ['id', 'username', 'bonus', 'operations']
+
+
+class OperationSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = Operation
+        fields = ['user', 'method', 'count', 'desc']
