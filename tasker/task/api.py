470aa904db76563d7478ef35bc67893dabb41a59
DollaR <cooler84@ukr.net>
2021-05-29 15:57:48 +0300

    create method set_data to clickhouse database

--- tasker/task/api.py
+++ tasker/task/api.py
@@ -7,9 +7,15 @@ Created on 27.05.2021
 
 """
 
-from sqlalchemy import create_engine
+import datetime
 
-from tasker.config import PostgresConfig
+from sqlalchemy import create_engine, MetaData
+
+from clickhouse_sqlalchemy import make_session
+
+from tasker.config import PostgresConfig, ClickHouseConfig
+
+from tasker.task.models import get_user_balance_model
 
 
 def get_users_data():
@@ -21,4 +27,20 @@ def get_users_data():
 
 
 def set_users_ch(data):
-    pass
+    db_string = f"clickhouse+native://{ClickHouseConfig.USER}:@{ClickHouseConfig.HOST}:{ClickHouseConfig.PORT}/{ClickHouseConfig.DBNAME}"
+    db = create_engine(db_string)
+    session = make_session(db)
+    metadata = MetaData(bind=db)
+    UserBalance = get_user_balance_model(metadata)
+
+    for user_id, balance in data.items():
+        b0 = b1000 = b100k = 0
+        if 0 == balance:
+            b0 = 1
+        elif 1000 <= balance:
+            b1000 = 1
+        elif 100000 <= balance:
+            b100k = 1
+        row = UserBalance(id=user_id, balance=balance, bonus0=b0, bonus1000=b1000, bonus100k=b100k, timestamp=datetime.datetime.now())
+        session.add(row)
+    session.commit()
